d666f71e297f8745251ab5450309f22976782d7e
Brian Bansenauer <brianb@cascadia.edu>
2013-05-28 15:07:36 -0700

    Deleted state duplicate

--- js/database.js
+++ js/database.js
@@ -1,5 +1,4 @@
         var song,
-            state,
             songList; 
 
         var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
