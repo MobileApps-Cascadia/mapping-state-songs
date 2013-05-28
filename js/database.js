d5ac7eae7d5d26b2e5adb7252188257d5c0c60f8
Brian Bansenauer <brianb@cascadia.edu>
2013-05-28 16:30:54 -0700

    Added a number of console.log calls

--- js/database.js
+++ js/database.js
@@ -1,12 +1,35 @@
-        var song,
-            songList; 
+var song,songList; 
+var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+// wait until the device is ready
+document.addEventListener("deviceready", onDeviceReady, false);
+// Cordova is ready
+//
+function onDeviceReady() {
+	console.log("device ready");
+	var db = window.openDatabase("StateTuning", "1.0", "StateTuning", 200000);
+	db.transaction(setUpDB, errorCB, successCB);
 
-        var deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+	$('.likeButton').click(function () {
+		song = $(this).parent().data('song');
+		//state = $('#state').data('state');
 
-        // wait until the device is ready
-        document.addEventListener("deviceready", onDeviceReady, false);
-
-        //Perform actions on device ready
+		db.transaction(likeSongDB, errorCB, successCB);
+		$.ajax({
+			url: "http://216.186.69.45/services/like_tune/" + song,
+			type: 'PUT',
+			success: function (response) {
+				console.log('PUT Completed');
+			}
+		})
+		 if (deviceType == 'Android') {
+			 $(this).attr("src",'/android_asset/www/images/redHeart.png');
+		}
+		else {
+			$(this).attr("src",'images/redHeart.png');
+		}
+		
+	});
+}
 
 
         function setUpDB(tx) {
@@ -50,39 +73,11 @@
             db.transaction(queryDB, errorCB);
         }
 
-        // Cordova is ready
-        //
-        function onDeviceReady() {
-            var db = window.openDatabase("StateTuning", "1.0", "StateTuning", 200000);
-            db.transaction(setUpDB, errorCB, successCB);
-
-            $('.likeButton').click(function () {
-                song = $(this).parent().data('song');
-                state = $('#state').data('state');
-
-                db.transaction(likeSongDB, errorCB, successCB);
-                $.ajax({
-                    url: "http://216.186.69.45/services/like_tune/" + song,
-                    type: 'PUT',
-                    success: function (response) {
-                        console.log('PUT Completed');
-                    }
-                })
-                 if (deviceType == 'Android') {
-                     $(this).attr("src",'/android_asset/www/images/redHeart.png');
-                }
-                else {
-                    $(this).attr("src",'images/redHeart.png');
-                }
-                
-            });
-        }
-
 
         //============================================================================================================
 function updateState()
 	{
-	
+	console.log("starting Update State");
 	$.ajax({
             type: 'GET',
             url: 'http://216.186.69.45/services/state_list/' + state,
@@ -113,10 +108,10 @@ function replacepage(tunes){
         var theWholeThing;
         for (I = 0; I < tunes.length; I++)
         {
-            theWholeThing += "<li id=\"song\" data-song=\"5\"><a href=\"#\" class=\"btn large\" onclick=\"playAudio('" + tunes[I].content + "');\"><img src=\"images/play.png\"></a><a href=\"#\" class=\"btn large\" onclick=\"pauseAudio();\"><img src=\"images/pause.png\"></a>" + tunes[I].content + " likes = " + tunes.likes + "</li>"
+            theWholeThing += "<li id=\"song\" data-song=\"5\"><a href=\"#\" class=\"btn large\" onclick=\"playAudio('http://216.186.69.45/access/" + tunes[I].content + "');\"><img src=\"images/play.png\"></a><a href=\"#\" class=\"btn large\" onclick=\"pauseAudio();\"><img src=\"images/pause.png\"></a>" + tunes[I].content + " likes = " + tunes.likes + "</li>";
 				            //"<li id=\"song\" data-song=\"5\"><a href=\"#\" class=\"btn large\" onclick=\"playAudio('" + songurl + "');\"><img src=\"images/play.png\"></a><a href=\"#\" class=\"btn large\" onclick=\"pauseAudio();\"><img src=\"images/pause.png\"></a>" + song.name + "</li>"
         }
-        document.getElementById('listOfSongs').innerHTML = theWholeThing;
+       $('#listOfSongs').html(theWholeThing);
 
 
         //picture
